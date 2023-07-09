5ae3fa9a8bdfedef2e5ba4fd31599153ea411422
emilioz <emilioz@za.velocitytrade.com>
2023-07-09 19:45:10 +0200

    simple flake with reference to openfortivpn

--- flake.nix
+++ flake.nix
@@ -2,21 +2,35 @@
   description = "A flake shell script to glue together `openfortivpn` and `openfortivpn-webview`";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    defaultPackage.x86_64-linux = with import nixpkgs {system = "x86_64-linux";};
-      stdenv.mkDerivation rec {
-        name = "emilio-test";
-        src = ./.;
-        buildPhase = "";
-        installPhase = ''
-          mkdir -p $out/bin
-          cp ${src}/test.sh $out/bin/emilio-test
-          chmod +x $out/bin/emilio-test
-        '';
-      };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        name = "vpn";
+        buildInputs = with pkgs; [
+          openfortivpn
+        ];
+        script = (pkgs.writeScriptBin name (builtins.readFile ./vpn.sh)).overrideAttrs (
+          old: {
+            buildCommand = "${old.buildCommand}\n patchShebangs $out";
+          }
+        );
+      in rec {
+        defaultPackage = packages.script;
+        packages.script = pkgs.symlinkJoin {
+          name = name;
+          paths = [script] ++ buildInputs;
+          buildInputs = [pkgs.makeWrapper];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+      }
+    );
 }
