04a883fdac08b701b6060421acd3402c032bb3e7
Emilio Ziniades <emilioz@za.velocitytrade.com>
2024-08-01 10:52:55 +0200

    chore: use shfmt to format vpn.sh and also use shellcheck to lint it

--- vpn.sh
+++ vpn.sh
@@ -6,31 +6,33 @@ VPN_NAME_FILE=~/.vpn.current
 VPN_PID=$(pgrep openfortivpn || true)
 
 main() {
-    if [[ ! -e $VPN_CFG_FILE ]]
-    then
+    if [[ ! -e $VPN_CFG_FILE ]]; then
         echo "please create a configuration file at ~/.vpn with at least one default"
         exit 1
     fi
 
     case $1 in
-        up)
-            up $2;;
-        down)
-            down;;
-        status)
-            status;;
-        logs)
-            logs;;
-        *)
-            echo "please specify a valid subcommand: up {vpn-name}, down, status"
+    up)
+        up "$2"
+        ;;
+    down)
+        down
+        ;;
+    status)
+        status
+        ;;
+    logs)
+        logs
+        ;;
+    *)
+        echo "please specify a valid subcommand: up {vpn-name}, down, status"
+        ;;
     esac
 }
 
 up() {
-    if [[ -z $VPN_PID ]]
-    then
-        if [[ -z $1 ]]
-        then
+    if [[ -z $VPN_PID ]]; then
+        if [[ -z $1 ]]; then
             echo "connecting to default vpn"
             host=$(jq -e -c 'map(select(.default == true)) | "\(.[0].host):\(.[0].port)"' $VPN_CFG_FILE | tr -d '"')
             name=$(jq -e -c 'map(select(.default == true)) | .[0].name' $VPN_CFG_FILE | tr -d '"')
@@ -39,19 +41,18 @@ up() {
             host=$(jq -e -c "map(select(.name == \"$1\")) | \"\(.[0].host):\(.[0].port)\"" $VPN_CFG_FILE | tr -d '"')
             name=$(jq -e -c "map(select(.name == \"$1\")) | .[0].name" $VPN_CFG_FILE | tr -d '"')
             cert=$(jq -e -c "map(select(.name == \"$1\")) | .[0].cert" $VPN_CFG_FILE | tr -d '"')
-            if [[ $? == 1 ]]
-            then
+            if [[ $? == 1 ]]; then
                 echo "could not find host $1, check the ~/.vpn config file"
                 exit 1
             fi
         fi
         echo "connecting to $name"
-        echo $name > $VPN_NAME_FILE
-        COOKIE=$(openfortivpn-webview $host)
-        if [ $cert = "null" ]; then
-          sudo -b openfortivpn --cookie=$COOKIE $host
+        echo "$name" >$VPN_NAME_FILE
+        COOKIE=$(openfortivpn-webview "$host")
+        if [ "$cert" = "null" ]; then
+            sudo -b openfortivpn --cookie="$COOKIE" "$host"
         else
-          sudo -b openfortivpn --cookie=$COOKIE $host --trusted-cert $cert
+            sudo -b openfortivpn --cookie="$COOKIE" "$host" --trusted-cert "$cert"
         fi
     else
         echo "vpn already connected"
@@ -59,23 +60,21 @@ up() {
 }
 
 down() {
-    if [[ -z $VPN_PID ]]
-    then
+    if [[ -z $VPN_PID ]]; then
         echo "vpn already down"
     else
         echo "stopping vpn"
-        sudo kill $VPN_PID
+        sudo kill "$VPN_PID"
         rm $VPN_NAME_FILE
     fi
 }
 
 status() {
-    if [[ -z $VPN_PID ]]
-    then
+    if [[ -z $VPN_PID ]]; then
         echo "[DOWN]"
     else
         echo "[UP] [PID: $VPN_PID] [VPN: $(cat $VPN_NAME_FILE)]"
     fi
 }
 
-main $1 $2
+main "$1" "$2"
